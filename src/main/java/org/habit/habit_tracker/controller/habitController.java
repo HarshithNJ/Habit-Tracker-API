549fbf98fdc8d3d46b36380d004585b6bc46f197
Harshith N J <njharshith122@gmail.com>
2025-01-03 19:01:09 +0530

    made changes in the code

--- src/main/java/org/habit/habit_tracker/controller/habitController.java
+++ src/main/java/org/habit/habit_tracker/controller/habitController.java
@@ -5,8 +5,16 @@ import org.habit.habit_tracker.service.habitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PatchExchange;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -17,8 +25,23 @@ public  class habitController{
     habitService service;
 
     @PostMapping("/habits")
-    public ResponseEntity<Object> saveHabit(@RequestBody habit hab){
-        return service.saveHabit(hab);
+    public ResponseEntity<Object> saveHabit(@RequestBody habit habit){
+        return service.saveHabit(habit);
+    }
+
+    @GetMapping("/habits")
+    public ResponseEntity<Object> getHabits(){
+        return service.getHabits();
+    }
+
+    @DeleteMapping("/habits/{id}")
+    public ResponseEntity<Object> deleteHabits(@PathVariable int id){
+        return service.deleteHabits(id);
+    }
+
+    @PatchMapping("/habits/{id}")
+    public ResponseEntity<Object> putMethodName(@PathVariable String id, @RequestBody habit habit) {
+        return service.putMethodName(id, habit);
     }
     
 }
--- src/main/java/org/habit/habit_tracker/service/habitService.java
+++ src/main/java/org/habit/habit_tracker/service/habitService.java
@@ -1,7 +1,9 @@
 package org.habit.habit_tracker.service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.habit.habit_tracker.dto.habit;
 import org.habit.habit_tracker.repository.habitRepository;
@@ -17,25 +19,56 @@ public class habitService {
     @Autowired
     habitRepository repository;
 
-    public ResponseEntity<Object> saveHabit(habit hab) {
-        if(repository.existsByCreatedOn(hab.getCreatedOn())){
-            
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("success", "Record successfully saved");
-            map.put("Data", hab);
-
-            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
-            
-        }else{
-
-            repository.save(hab);
+    public ResponseEntity<Object> saveHabit(habit habit) {
+            repository.save(habit);
 
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("success", "Record successfully saved");
-            map.put("Data", hab);
+            map.put("Data", habit);
 
             return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Object> getHabits() {
+        List<habit> habits = repository.findAll();
+        
+        if(habits.isEmpty()){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "No Habits Found");
+
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }else{
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Habits Found");
+            map.put("Data", habits);
+
+            return new ResponseEntity<Object>(map, HttpStatus.OK);
         }
     }
+
+    public ResponseEntity<Object> deleteHabits(int id) {
+        Optional<habit> habit = repository.findById(id);
+
+        if(habit.isPresent()){
+            repository.deleteById(id);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Record deleted successfully");
+            map.put("Data", habit);
+
+            return new ResponseEntity<Object>(map, HttpStatus.ACCEPTED);
+        }else{
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Record not found");
+
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    public ResponseEntity<Object> putMethodName(String id, habit habit) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'putMethodName'");
+    }
+
     
 }
